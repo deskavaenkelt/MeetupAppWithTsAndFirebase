0d0ad77c0e40dccd569ee0027f62b35ad009aa2c
Lars Strömberg <stromberg.lars@gmail.com>
2022-05-10 18:13:39 +0200

    Break out Todo component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Todo from './components/Todo'
 
 function App() {
 	return (
 		<>
 			<h1>Todo App</h1>
-		
+			<Todo />
 		</>
 	)
 }
---
+++ client/src/components/Todo.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Todo = () => {
+	
+	return (
+		<div className='card'>
+			<h2>Titel</h2>
+			<div className='actions'>
+				<button className='btn'>Delete</button>
+			</div>
+		</div>
+	)
+}
+
+export default Todo
