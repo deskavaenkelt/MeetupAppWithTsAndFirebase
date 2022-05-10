fb15ef9da27ade6292a7bdfd0a74b0da3bafb81b
Lars Strömberg <stromberg.lars@gmail.com>
2022-05-11 00:29:22 +0200

    Favorites badge

--- client/src/components/navigation/Navigation.tsx
+++ client/src/components/navigation/Navigation.tsx
@@ -1,16 +1,23 @@
 import { Link } from 'react-router-dom'
 import RoutingPath from '../../routes/RoutingPath'
 import css from './Navigation.module.css'
+import { useContext } from 'react'
+import FavoritesContext from '../../utils/provider/favoritesContext'
 
 const Navigation = () => {
+	const favoritesContext = useContext(FavoritesContext)
 	
 	return (
-		<nav className={css.header}>
-			<div className={css.logo}>React Meetups</div>
+		<nav className={ css.header }>
+			<div className={ css.logo }>React Meetups</div>
 			<ul>
-				<li><Link to={RoutingPath.allMeetupsView}>All Meetups</Link></li>
-				<li><Link to={RoutingPath.newMeetupView}>Add New Meetup</Link></li>
-				<li><Link to={RoutingPath.favoritesView}>Favorites</Link></li>
+				<li><Link to={ RoutingPath.allMeetupsView }>All Meetups</Link></li>
+				<li><Link to={ RoutingPath.newMeetupView }>Add New Meetup</Link></li>
+				<li>
+					<Link to={ RoutingPath.favoritesView }>
+						Favorites <span className={ css.badge }>{ favoritesContext?.totalFavorites }</span>
+					</Link>
+				</li>
 			</ul>
 		</nav>
 	)
